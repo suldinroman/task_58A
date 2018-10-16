3e04e4b91bd4cc47a63c3fef36bd2452577842ab
suldinroman <suldinroman@yandex.ru>
2018-10-16 12:35:21 +0300

    First upload.

---
+++ task_58A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string inputString;
+    std::string helloString = "hello";
+
+    unsigned int charactersFound = 0;
+    unsigned int position = 0;
+
+    std::cin >> inputString;
+
+    while (charactersFound < 5 && position < inputString.length())
+	{
+        if (inputString.find(helloString[charactersFound], position) == std::string::npos)
+            break;
+
+        position = inputString.find(helloString[charactersFound], position) + (charactersFound == 2 ? 1 : 0);
+        ++charactersFound;
+	}
+
+    std::cout << (charactersFound == 5 ? "YES" : "NO") << std::endl;
+}
